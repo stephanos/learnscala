03bc3c5e99e6ffb971274efceffa044d321b3dd7
Stephan <stephan.behnke@googlemail.com>
2012-07-09 18:19:50 +0200

    commit

--- app/controllers/Slide.scala
+++ app/controllers/Slide.scala
@@ -24,17 +24,17 @@ object Slide extends MyController {
     def load(id: String) =
         load2(null, id)
 
-    def load2(id1: String, id2: String) = Action {
-        try {
-            val dir = if (id1 == null) "" else id1.replaceAllLiterally("-", "_") + "."
-            val file = id2.replaceAllLiterally("-", "_")
+def load2(id1: String, id2: String) = Action {
+    try {
+        val dir = if (id1 == null) "" else id1.replaceAllLiterally("-", "_") + "."
+        val file = id2.replaceAllLiterally("-", "_")
 
-            val c = Class.forName("views.html.slides." + dir + file)
-            val m = c.getMethod("render")
-            Ok(m.invoke(null).asInstanceOf[Html])
-        } catch {
-            case e =>
-                Ok(views.html.error.notfound())
-        }
+        val c = Class.forName("views.html.slides." + dir + file)
+        val m = c.getMethod("render")
+        Ok(m.invoke(null).asInstanceOf[Html])
+    } catch {
+        case e =>
+            Ok(views.html.error.notfound())
     }
+}
 }
