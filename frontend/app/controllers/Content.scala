ded0e2bb866fb9692837e75eeca5661a8ba65561
stephan <stephan@101loops.com>
2012-09-12 11:22:26 +0200

    commit

--- frontend/app/controllers/Content.scala
+++ frontend/app/controllers/Content.scala
@@ -4,6 +4,11 @@ import controllers.base.MyController
 
 object Content extends MyController {
 
+    val fp = "fp"
+    val oop = "oop"
+    val ext = "ext"
+    val basic = "basic"
+
     case class Slide(id: String, label: String, chpt: String, deps: Seq[Slide] = Seq(), status: String = "planned")
 
     // ================================================================================================================
@@ -11,25 +16,26 @@ object Content extends MyController {
 
     // === ALLGEMEIN
 
-    lazy val einfuehrung = Slide("B10", "Einführung", "basic", status = "")
-    lazy val geschichte = Slide("B11", "Geschichte", "basic", Seq(einfuehrung), status = "")
-    lazy val ersteSchritte = Slide("B12", "Erste Schritte", "basic", Seq(tools1), status = "")
-    lazy val kontrollstrukturen = Slide("B13", "Kontrollstrukturen", "basic", Seq(tools2), status = "")
+    lazy val einfuehrung = Slide("B10", "Einführung", basic, status = "")
+    lazy val geschichte = Slide("B11", "Geschichte", basic, Seq(einfuehrung), status = "")
+    lazy val ersteSchritte = Slide("B12", "Erste Schritte", basic, Seq(werkzeuge1), status = "")
+    lazy val kontrollstrukturen = Slide("B13", "Kontrollstrukturen", basic, Seq(werkzeuge2), status = "")
 
     // === OBJEKTE
-    lazy val klassen = Slide("O10", "Klassen", "oop", Seq(), status = "")
-    lazy val vererbung1 = Slide("O11", "Vererbung I", "oop", Seq(klassen), status = "")
-    lazy val objekte = Slide("O12", "Objekte", "oop", Seq(vererbung1), status = "")
-    lazy val sichtbarkeit = Slide("O13", "Sichtbarkeit", "oop", Seq(objekte), status = "")
-    lazy val pakete = Slide("O14", "Pakete", "oop", Seq(sichtbarkeit), status = "")
+    lazy val klassen = Slide("O10", "Klassen", oop, Seq(), status = "")
+    lazy val vererbung1 = Slide("O11", "Vererbung I", oop, Seq(klassen), status = "")
+    lazy val objekte = Slide("O12", "Objekte", oop, Seq(vererbung1), status = "")
+    lazy val sichtbarkeit = Slide("O13", "Sichtbarkeit", oop, Seq(objekte), status = "")
+    lazy val pakete = Slide("O14", "Pakete", oop, Seq(sichtbarkeit), status = "")
 
     // === FUNKTIONEN
 
 
     // === ERWEITERT
 
-    lazy val tools1 = Slide("E10", "Werkzeuge I", "ext", Seq(geschichte), status = "")
-    lazy val tools2 = Slide("E11", "Werkzeuge II", "ext", Seq(ersteSchritte), status = "")
+    lazy val werkzeuge1 = Slide("E10", "Werkzeuge I", ext, Seq(geschichte), status = "")
+    lazy val werkzeuge2 = Slide("E11", "Werkzeuge II", ext, Seq(ersteSchritte), status = "")
+    lazy val uebungen = Slide("E12", "Übungen", ext, Seq(werkzeuge2), status = "")
 
 
     // ================================================================================================================
@@ -37,12 +43,13 @@ object Content extends MyController {
 
     // === ALLGEMEIN
 
-    lazy val recursion = Slide("B25", "Rekursion", "basic", status = "incomplete")
+    lazy val recursion = Slide("B25", "Rekursion", basic, status = "incomplete")
 
 
     // === OBJEKTE
-    lazy val hierarchy = Slide("O20", "Hierarchie", "oop", status = "")
-    lazy val traits = Slide("O21", "Traits", "oop", status = "")
+
+    lazy val hierarchy = Slide("O20", "Hierarchie", oop, status = "")
+    lazy val traits = Slide("O21", "Traits", oop, status = "")
 
 
     // === FUNKTIONEN
@@ -63,6 +70,8 @@ object Content extends MyController {
 
 
     // === FUNKTIONEN
+
+    lazy val kollektionen2 = Slide("F030", "Kollektionen II", "fp", status = "incomplete")
 
 
     // === ERWEITERT
