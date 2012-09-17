c8fa8a47afa4dbc3684826598f47a4c244ac0da5
stephan <stephan@101loops.com>
2012-09-17 15:03:47 +0200

    commit

--- frontend/app/views/Scribble.scala
+++ frontend/app/views/Scribble.scala
@@ -1,1 +1,15 @@
 package views
+
+class EUR(val v: Float)
+class USD(val v: Float)
+
+object Cashier {
+
+    def pay(x: EUR) {
+        println("payed %s€".format(x.v))
+    }
+
+    def pay(x: USD) {
+        println("payed %s$".format(x.v))
+    }
+}
