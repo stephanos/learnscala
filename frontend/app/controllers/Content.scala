4324ca274385c755dd0ec2caf9c00f5bc2323834
stephan <stephan@101loops.com>
2012-10-11 21:11:46 +0200

    commit

--- frontend/app/controllers/Content.scala
+++ frontend/app/controllers/Content.scala
@@ -65,7 +65,7 @@ object Content extends MyController {
 
     lazy val datatypes1 = Slide("F20", "Datenstrukturen I", fp, Seq(generics1))
     lazy val datatypes2 = Slide("F21", "Datenstrukturen II", fp, Seq(datatypes1), status = "complete")
-    lazy val listen = Slide("F22", "Listenverarbeitung", fp, Seq(datatypes2))
+    lazy val listen = Slide("F22", "Listenverarbeitung", fp, Seq(datatypes2), status = "complete")
     lazy val funktionen3 = Slide("F23", "Funktionen III", fp, Seq(datatypes2, funktionen2))
     lazy val forexpr = Slide("F24", "for-Ausdruck", fp, Seq(funktionen3), status = "complete")
 
