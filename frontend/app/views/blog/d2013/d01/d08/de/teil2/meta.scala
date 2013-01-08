2b843e0c5e509cc64a8a75631bfd7eab3a20fa8a
stephan <stephan@101loops.com>
2013-01-08 16:24:57 +0100

    commit

--- frontend/app/views/blog/d2013/d01/d08/de/teil2/meta.scala
+++ frontend/app/views/blog/d2013/d01/d08/de/teil2/meta.scala
@@ -1,9 +1,9 @@
 package views.html.blog
-package d2013.d01.d08.de.part2
+package d2013.d01.d08.de.teil2
 
 object meta extends BlogEntry {
 
     override val langs = List("en", "de")
 
-    val title = "Scala im Unternehmen: Part 2"
+    val title = "Scala im Unternehmen: Teil 2"
 }
